9ba50839a1c47d70e6225023f28802f629b7c781
QuoteCC <colin.corliss@gmail.com>
2016-12-06 17:21:06 -0500

    Code Refactor

--- www/cgi-bin/recReply.py
+++ www/cgi-bin/recReply.py
@@ -21,7 +21,7 @@ content = messageInfo['RepId'].value
 
 conn = mysql.connector.connect(user='DCC', password='abcd', database='Ocean')
 cursor = conn.cursor()
-query = "SELECT Title, Data, Sent, Posttime, ReplyId FROM Replies WHERE ReplyId='"+content+"'"
+query = "SELECT Title, Data, Sent, Posttime, ReplyId FROM Replies WHERE mID='"+content+"'"
 cursor.execute(query)
 result = cursor.fetchall()
 result = result[0]
@@ -31,19 +31,6 @@ conn.close()
 
 
 
-# loopCounter = 0;
-# while "\"" in result or "\'" in result: #Cleanse results of quotes
-# 	result[result.index("\"")] = "\\"+"\""
-# 	result[result.index("\'")] = "\\"+"\'"
-# 	if loopCounter > len(result):  #Basic inf loop failsafe
-# 		break;
-
-#formattedResult = ""
-#for column in result:
-#	formattedResult += str(column)
-#	formattedResult += ","
-#formattedResult = formattedResult[:-1]
-#Format into comma seperated list
 
 print ("Content-type: text/html\n\n")
 print json.dumps(jsonreturn)
